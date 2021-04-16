4cedf3c45f0a62d8618daff49b110551dbe75728
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2021-04-16 22:02:07 +0200

    Could redirect to orders list

--- behaviors/OrderFakturoidSendable.php
+++ behaviors/OrderFakturoidSendable.php
@@ -1,19 +1,27 @@
 <?php namespace VojtaSvoboda\ShopaholicFakturoid\Behaviors;
 
+use Backend;
 use Exception;
 use Flash;
 use October\Rain\Extension\ExtensionBase;
+use Redirect;
+use VojtaSvoboda\Fakturoid\Models\Settings;
 use VojtaSvoboda\ShopaholicFakturoid\Services\OrderService;
 
 class OrderFakturoidSendable extends ExtensionBase
 {
     /**
      * @param int $order_id
+     * @return Redirect
      */
     public function onFakturoidSend($order_id)
     {
         /** @var OrderService $service */
         $service = app(OrderService::class);
+
+        // init
+        $error = null;
+        $redirect = Backend::url('lovata/ordersshopaholic/orders/update/' . $order_id . '?#primarytab-fakturoid');
 
         try {
             // create new invoice
@@ -24,7 +32,20 @@ class OrderFakturoidSendable extends ExtensionBase
             if (empty($error)) {
                 $error = 'Neznámá chyba. Více info viz. Fakturoid > Log';
             }
+
             Flash::error($error);
+
+            return Redirect::to($redirect);
         }
+
+        // redirect to the orders list
+        $toList = Settings::get('settings_redirect_to_list', false);
+        if (!empty($toList) && !empty($error)) {
+            $redirect = Backend::url('lovata/ordersshopaholic/orders');
+        }
+
+        Flash::success('Objednávka úspěšné odeslána.');
+
+        return Redirect::to($redirect);
     }
 }
